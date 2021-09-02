fd3d77cd7486c1fcf5df5fb7387d2cb546f5f4e3
Phong <phong940253@gmail.com>
2021-09-02 09:04:30 +0700

    clean layout & show content

--- resources/views/users/tieuchuan.blade.php
+++ resources/views/users/tieuchuan.blade.php
@@ -15,86 +15,87 @@
             <div class="col-xl-8 order-xl-1">
                 <div class="card bg-secondary shadow">
                     <div class="card-header bg-white border-0">
-                        <div class="row align-items-center">
-                            <h3 class="mb-0 ml-3">{{ __('Thông tin chung') }}</h3>
+                        <div class="nav-wrapper">
+    {{--                            <ul class="nav nav-pills nav-pills-circle" id="tabs_2" role="tablist">--}}
+    {{--                                <li class="nav-item">--}}
+    {{--                                    <a class="nav-link rounded-circle active" id="home-tab" data-toggle="tab" href="#tabs_2_1" role="tab" aria-controls="home" aria-selected="true">--}}
+    {{--                                        <span class="nav-link-icon d-block"><i class="ni ni-atom"></i></span>--}}
+    {{--                                    </a>--}}
+    {{--                                </li>--}}
+    {{--                                <li class="nav-item">--}}
+    {{--                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#tabs_2_2" role="tab" aria-controls="profile" aria-selected="false">--}}
+    {{--                                        <span class="nav-link-icon d-block"><i class="ni ni-chat-round"></i></span>--}}
+    {{--                                    </a>--}}
+    {{--                                </li>--}}
+    {{--                                <li class="nav-item">--}}
+    {{--                                    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#tabs_2_3" role="tab" aria-controls="contact" aria-selected="false">--}}
+    {{--                                        <span class="nav-link-icon d-block"><i class="ni ni-cloud-download-95"></i></span>--}}
+    {{--                                    </a>--}}
+    {{--                                </li>--}}
+    {{--                            </ul>--}}
+                            <ul class="nav nav-pills nav-pills-circle flex-column flex-md-row" id="tabs-icons-text" role="tablist">
+                                @for ($i = 1; $i <= count($noidungs); $i++)
+                                <li class="nav-item">
+                                    <a class="nav-link mb-sm-3 mb-md-2 {{($i == 1) ? 'active' : ''}}" id="tabs-icons-text-{{$i}}-tab" data-toggle="tab"
+                                       href="#tabs-icons-text-{{$i}}" role="tab" aria-controls="tabs-icons-text-{{$i}}"
+                                       aria-selected="{{($i == 1) ? 'true' : 'false'}}">{{$i}}</a>
+                                </li>
+                                @endfor
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-2-tab" data-toggle="tab"--}}
+{{--                                       href="#tabs-icons-text-2" role="tab" aria-controls="tabs-icons-text-2"--}}
+{{--                                       aria-selected="false">2</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-3-tab" data-toggle="tab"--}}
+{{--                                       href="#tabs-icons-text-3" role="tab" aria-controls="tabs-icons-text-3"--}}
+{{--                                       aria-selected="false">3</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-1-tab" data-toggle="tab"--}}
+{{--                                       href="#tabs-icons-text-1" role="tab" aria-controls="tabs-icons-text-1"--}}
+{{--                                       aria-selected="true">1</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-2-tab" data-toggle="tab"--}}
+{{--                                       href="#tabs-icons-text-2" role="tab" aria-controls="tabs-icons-text-2"--}}
+{{--                                       aria-selected="false">2</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-3-tab" data-toggle="tab"--}}
+{{--                                       href="#tabs-icons-text-3" role="tab" aria-controls="tabs-icons-text-3"--}}
+{{--                                       aria-selected="false">3</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-1-tab" data-toggle="tab"--}}
+{{--                                       href="#tabs-icons-text-1" role="tab" aria-controls="tabs-icons-text-1"--}}
+{{--                                       aria-selected="true">1</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-2-tab" data-toggle="tab"--}}
+{{--                                       href="#tabs-icons-text-2" role="tab" aria-controls="tabs-icons-text-2"--}}
+{{--                                       aria-selected="false">2</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-3-tab" data-toggle="tab"--}}
+{{--                                       href="#tabs-icons-text-3" role="tab" aria-controls="tabs-icons-text-3"--}}
+{{--                                       aria-selected="false">3</a>--}}
+{{--                                </li>--}}
+                            </ul>
                         </div>
                     </div>
-                    <div class="card-body">
-                        <form method="post" action="{{ route('profile.update') }}" autocomplete="off">
-                            @csrf
-                            @method('put')
 
-                            <h6 class="heading-small text-muted mb-4">{{ __('Người dùng') }}</h6>
-
-                            @if (session('status'))
-                                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                    {{ session('status') }}
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                            @endif
-
-
-                            <div class="pl-lg-4">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="tab-content" id="myTabContent">
+                                @for ($i = 1; $i <= count($noidungs); $i++)
+                                    <div class="tab-pane fade show {{($i == 1) ? 'active' : ''}}" id="tabs-icons-text-{{$i}}" role="tabpanel"
+                                         aria-labelledby="tabs-icons-text-{{$i}}-tab">
+                                        <p class="description">{{$noidungs[$i - 1]->content}}</p>
+                                    </div>
+                                @endfor
                             </div>
-                        </form>
-                        <hr class="my-4"/>
-                        <form method="post" action="{{ route('profile.password') }}" autocomplete="off">
-                            @csrf
-                            @method('put')
-
-                            <h6 class="heading-small text-muted mb-4">{{ __('Mật khẩu') }}</h6>
-
-                            @if (session('password_status'))
-                                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                    {{ session('password_status') }}
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                            @endif
-
-                            <div class="pl-lg-4">
-                                <div class="form-group{{ $errors->has('old_password') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label"
-                                           for="input-current-password">{{ __('Mật khẩu hiện tại') }}</label>
-                                    <input type="password" name="old_password" id="input-current-password"
-                                           class="form-control form-control-alternative{{ $errors->has('old_password') ? ' is-invalid' : '' }}"
-                                           placeholder="{{ __('Mật khẩu hiện tại') }}" value="" required>
-
-                                    @if ($errors->has('old_password'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('old_password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                                <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label"
-                                           for="input-password">{{ __('Mật khẩu mới') }}</label>
-                                    <input type="password" name="password" id="input-password"
-                                           class="form-control form-control-alternative{{ $errors->has('password') ? ' is-invalid' : '' }}"
-                                           placeholder="{{ __('Mật khẩu mới') }}" value="" required>
-
-                                    @if ($errors->has('password'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                                <div class="form-group">
-                                    <label class="form-control-label"
-                                           for="input-password-confirmation">{{ __('Nhập lại mật khẩu mới') }}</label>
-                                    <input type="password" name="password_confirmation" id="input-password-confirmation"
-                                           class="form-control form-control-alternative"
-                                           placeholder="{{ __('Nhập lại mật khẩu mới') }}" value="" required>
-                                </div>
-
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-success mt-4">{{ __('Đổi mật khẩu') }}</button>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
