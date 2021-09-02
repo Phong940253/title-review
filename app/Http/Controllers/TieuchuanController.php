332fe0071d2005ba4493a990784bc5ce6f4000a1
Phong <phong940253@gmail.com>
2021-09-02 09:03:23 +0700

    complete tieuchuan controller

--- app/Http/Controllers/TieuchuanController.php
+++ app/Http/Controllers/TieuchuanController.php
@@ -4,12 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
+/**
+ *
+ */
 class TieuchuanController extends Controller
 {
     //
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index(Request $request) {
         $tieuchis = ProfileController::getTieuChuanTieuChi($request);
-        return view('users.tieuchuan', ['tieuchis' => $tieuchis]);
+        $noidungs = $this->getNoiDung($request);
+        return view('users.tieuchuan', ['tieuchis' => $tieuchis, 'noidungs' => $noidungs]);
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public static function getNoiDung(Request $request) {
+        $id_tieuchi = $request->id_tieuchi;
+        $id_tieuchuan = $request->id_tieuchuan;
+        if ($id_tieuchi || $id_tieuchuan) {
+            if (!$id_tieuchuan) {
+                $res = DB::table('noidung')
+                    ->join('tieuchi', 'noidung.id_tieuchi', '=', 'tieuchi.id')
+                    ->get();
+            }
+            $res = DB::table('noidung')
+                ->join('tieuchuan', 'noidung.id_tieuchuan', '=', 'tieuchuan.id')
+                ->join('tieuchi', 'tieuchuan.id_tieuchi', '=', 'tieuchi.id')
+                ->where('tieuchuan.id', '=', $id_tieuchuan)
+                ->where('tieuchi.id', '=', $id_tieuchi)
+                ->get();
+            Log::debug($res);
+            return $res;
+        }
+        return [];
     }
 }
