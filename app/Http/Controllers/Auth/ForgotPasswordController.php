ed9274b54ba58553549cb69371a2d4775c7d9a97
Phong <phong940253@gmail.com>
2021-11-18 13:42:13 +0700

    change mail reset password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -19,4 +21,5 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
 }
